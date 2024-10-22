6fcf38b5b73bff92fcbefc7df1c27ea0b88ce16d
alamin2811 <alaminbinmonsur@gmail.com>
2024-10-22 23:59:51 +0600

    modified

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -62,14 +62,68 @@ if (cloud2.length) {
     });
 }
 
-/*--  --*/
-var cloud2 = $(".profile-cloud-3");
-if (cloud2.length) {
+/*-- join rocket img --*/
+var joinRocketImg = $(".join-rocket-img");
+if (joinRocketImg.length) {
     $(window).on("scroll", function () {
-        var cloud2 = $(".profile-cloud-3");
+        var joinRocketImg = $(".join-rocket-img");
         var y = window.scrollY;
         var x;
-        x = cloud2.offset().top;
+        x = joinRocketImg.offset().top;
+        x = x - 400;
+
+        let animationValue = 1;
+        animationValue = (y - x) / 9;
+
+        let animationStop = 100;
+
+        if (animationValue < 0) {
+            animationValue > 0;
+        }
+
+        if (animationValue > animationStop) {
+            animationValue = animationStop;
+        }
+
+        joinRocketImg.css("transform", `translateX(${animationValue}px)`);
+    });
+}
+
+/*-- bannerCloud img --*/
+var bannerCloud1 = $(".bannerCloud1");
+if (bannerCloud1.length) {
+    $(window).on("scroll", function () {
+        var bannerCloud1 = $(".bannerCloud1");
+        var y = window.scrollY;
+        var x;
+        x = bannerCloud1.offset().top;
+        x = x - 400;
+
+        let animationValue = 1;
+        animationValue = (y - x) / 9;
+
+        let animationStop = 100;
+
+        if (animationValue < 0) {
+            animationValue > 0;
+        }
+
+        if (animationValue > animationStop) {
+            animationValue = animationStop;
+        }
+
+        bannerCloud1.css("transform", `translateX(${animationValue}px)`);
+    });
+}
+
+/*-- bannerCloud img --*/
+var bannerCloud2 = $(".bannerCloud2");
+if (bannerCloud2.length) {
+    $(window).on("scroll", function () {
+        var bannerCloud2 = $(".bannerCloud2");
+        var y = window.scrollY;
+        var x;
+        x = bannerCloud2.offset().top;
         x = x - 400;
 
         let animationValue = 1;
@@ -85,8 +139,143 @@ if (cloud2.length) {
             animationValue = animationStop;
         }
 
-        cloud2.css("transform", `translateX(${animationValue}px)`);
+        bannerCloud2.css("transform", `translateX(${animationValue}px)`);
     });
 }
+/*-- profile-coin3 --*/
+var profileCoin3 = $(".profile-coin3");
+if (profileCoin3.length) {
+    $(window).on("scroll", function () {
+        var profileCoin3 = $(".profile-coin3");
+        var y = window.scrollY;
+        var x;
+        x = profileCoin3.offset().top;
+        x = x - 400;
+
+        let animationValue = 1;
+        animationValue = (y - x) / 9;
+
+        let animationStop = 100;
+
+        if (animationValue < 0) {
+            animationValue > 0;
+        }
+
+        if (animationValue > animationStop) {
+            animationValue = animationStop;
+        }
+
+        profileCoin3.css("transform", `translateX(${animationValue}px)`);
+    });
+}
+
+/*-- bannerCloud img --*/
+var bannerCloud2 = $(".bannerCloud2");
+if (bannerCloud2.length) {
+    $(window).on("scroll", function () {
+        var bannerCloud2 = $(".bannerCloud2");
+        var y = window.scrollY;
+        var x;
+        x = bannerCloud2.offset().top;
+        x = x - 400;
+
+        let animationValue = 1;
+        animationValue = (-1 * (y - x)) / 9;
+
+        let animationStop = 100;
+
+        if (animationValue < 0) {
+            animationValue > 0;
+        }
+
+        if (animationValue > animationStop) {
+            animationValue = animationStop;
+        }
+
+        bannerCloud2.css("transform", `translateX(${animationValue}px)`);
+    });
+}
+
+/*-- Team-coin --*/
+var teamCoin = $(".team-coin");
+if (teamCoin.length) {
+    $(window).on("scroll", function () {
+        var teamCoin = $(".team-coin");
+        var y = window.scrollY;
+        var x;
+        x = teamCoin.offset().top;
+        x = x - 400;
+
+        let animationValue = 1;
+        animationValue = (y - x) / 9;
+
+        let animationStop = 100;
+
+        if (animationValue < 0) {
+            animationValue > 0;
+        }
+
+        if (animationValue > animationStop) {
+            animationValue = animationStop;
+        }
+
+        teamCoin.css("transform", `translateX(${animationValue}px)`);
+    });
+}
+
+/*-- Team-coin-bib --*/
+var coinBibImg = $(".coin-bib-img");
+if (coinBibImg.length) {
+    $(window).on("scroll", function () {
+        var coinBibImg = $(".coin-bib-img");
+        var y = window.scrollY;
+        var x;
+        x = coinBibImg.offset().top;
+        x = x - 400;
+
+        let animationValue = 1;
+        animationValue = (-1 * (y - x)) / 9;
+
+        let animationStop = 100;
+
+        if (animationValue < 0) {
+            animationValue > 0;
+        }
+
+        if (animationValue > animationStop) {
+            animationValue = animationStop;
+        }
+
+        coinBibImg.css("transform", `translateX(${animationValue}px)`);
+    });
+}
+
+/*-- footer Ship-bib --*/
+var footerShip = $(".footer-ship");
+if (footerShip.length) {
+    $(window).on("scroll", function () {
+        var footerShip = $(".footer-ship");
+        var y = window.scrollY;
+        var x;
+        x = footerShip.offset().top;
+        x = x - 400;
+
+        let animationValue = 1;
+        animationValue = (-1 * (y - x)) / 6;
+
+        let animationStop = 100;
+
+        if (animationValue < 0) {
+            animationValue > 0;
+        }
+
+        if (animationValue > animationStop) {
+            animationValue = animationStop;
+        }
+
+        footerShip.css("transform", `translateX(${animationValue}px)`);
+    });
+}
+
 
 })(jQuery);
